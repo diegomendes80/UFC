0c63343a57226236ed44847fc052b0a35484f1c0
Diego-M <mendesdiego818@gmail.com>
2024-10-23 14:19:11 -0300

    Primeira aula de Java

--- POO - Java/helloworld/src/Main.java
+++ POO - Java/helloworld/src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello World!");
+
+
+        int[] numeros = {2, 6, 1, 7, 3};
+        int maiorNumero=0;
+        int menorNumero=0;
+
+        for(int i=0; i< numeros.length; i++){
+            if(i==0) maiorNumero = numeros[0];
+            else if(numeros[i] > maiorNumero) maiorNumero = numeros[i];
+
+        }
+
+        System.out.println(maiorNumero);
+
+
     }
+
+
+
+
 }
 
