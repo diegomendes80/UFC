c218bfd01ca7f6872fe24357c7196077c121eddf
Diego-M <mendesdiego818@gmail.com>
2024-11-18 15:43:19 -0300

    aula de java uf

---
+++ POO - Java/Aula 04/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Produto produto = new Produto();
+        produto.nomeProduto = "Playstation 5";
+        System.out.println(produto.nomeProduto);
+    }
+}
