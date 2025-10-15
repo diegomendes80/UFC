f10cd06ea6ed5c9ca9866d867cba0e6512772ab1
Diego Mendes <mendesdiego818@gmail.com>
2025-10-15 19:44:52 -0300

    aulas kicad e vhdl

--- VHDL/uc/uc.vhd
+++ VHDL/uc/uc.vhd
@@ -3,8 +3,12 @@ use ieee.std_logic_1164.all;
 
 entity uc is
     port (
-       entrada : in std_logic_vector(7 downto 0);
-       saida : out std_logic_vector(7 downto 0);
+        clk : in std_logic;
+        rst : in std_logic;
+        entrada : in std_logic_vector(7 downto 0);
+        op_a : in std_logic_vector(7 downto 0);
+        op_b : in std_logic_vector(7 downto 0);
+        saida : out std_logic_vector(7 downto 0);
 
     );
 end uc;
@@ -12,8 +16,12 @@ end uc;
 
 architecture logic of uc is
 
-    signal ula_entrada : std_logic_vector(7 downto 0);
-    signal ula_saida : std_logic_vector(7 downto 0);
+    signal ula_a : std_logic_vector(7 downto 0);
+    signal ula_b : std_logic_vector(7 downto 0);
+    signal ula_op : std_logic_vector(2 downto 0);
+    signal ula_cin : std_logic;
+    signal ula_cout : std_logic;
+    signal ula_result : std_logic_vector(7 downto 0);
     signal reg_we : std_logic;
     signal reg_data : std_logic_vector(7 downto 0);
     signal reg_addr : std_logic_vector(1 downto 0);
@@ -22,24 +30,30 @@ architecture logic of uc is
     signal reg_q2 : std_logic_vector(7 downto 0);
     signal reg_q3 : std_logic_vector(7 downto 0);
 
-    ula_inst.entity work.ULA
-    port map (
-        entrada => ula_entrada;
-        saida => ula_saida;
-    );
+    begin
+
+    ula_inst : entity  work.ULA
+        port map (
+            a => ula_a;
+            b => ula_b;
+            op => ula_op;
+            cin => ula_cin;
+            cout => ula_cout;
+            s => ula_result;
+        );
 
     reg_inst.entity work.REG
-    port map (
-            clk => clk;
-            rst => rst;
-            we  => reg_we;
-            data  => reg_data;
-            addr => reg_addr;
-            q0  => reg_q0;
-            q1 => reg_q1;
-            q2 => reg_q2;
-            q3 => reg_q3;
-    );
+        port map (
+                clk => clk;
+                rst => rst;
+                we  => reg_we;
+                data  => reg_data;
+                addr => reg_addr;
+                q0  => reg_q0;
+                q1 => reg_q1;
+                q2 => reg_q2;
+                q3 => reg_q3;
+        );
 
  
 
